d89e8f363befe0bdbef380a888baada8fa446890
praveenaj <jpraveena652@gmail.com>
2024-09-05 12:34:21 +0530

    EvenorOdd Program added

---
+++ javaPrograms/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package javaPrograms;
+import java.util.Scanner;
+
+public class EvenOrOdd {
+	 public static void main(String[] args) {
+	 Scanner scanner = new Scanner(System.in);
+	 System.out.print("Enter any number: ");
+	 int number = scanner.nextInt();
+	 if (number % 2 == 0) {
+	 System.out.println(number + " is even.");
+	 } else {
+	 System.out.println(number + " is odd.");
+	 }
+	 }
+	}
+
+
